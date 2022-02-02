8e09921c864aa811a63cac13685bd8625135b03b
ipari <me@ipari.dev>
2022-02-02 20:58:37 +0900

    gunicorn 으로 서빙하도록 변경

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import threading
-
 from app import create_app
 from app.sauron import Sauron
 
 sauron = Sauron()
 app = create_app(sauron)
 
-# thread = threading.Thread(target=sauron.watch, args=())
-# thread.daemon = True
-# thread.start()
 
 if __name__ == '__main__':
-    # app.run(threaded=True, host="0.0.0.0", port=5002)
-    app.run(host='0.0.0.0', port=5002, use_reloader=False)
+    app.run()
+
